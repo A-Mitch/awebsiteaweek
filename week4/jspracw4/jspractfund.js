04e2f2d409020f1fb9ed9231c4ab30a73fbfd251
Alexander Mitchell <amitchellofficial@gmail.com>
2019-10-07 11:33:51 -0400

    adds week4

---
+++ week4/jspracw4/jspractfund.js
@@ -0,0 +1,25 @@
+// This is where I will be practicing fundamentals of JS
+let admin, name;
+name = 'Alex';
+admin = name;
+alert(admin);
+
+// multiline declarations
+let food = 'almond',
+    allergic = true,
+    wouldibanthem = 'Yes';
+
+    // We cna use $ and _
+    let $ = 'dolla'
+    let _ = 'score'
+    alert($ + " " + _)
+
+    // inmutable
+
+    // lowercase because it is not known before running time
+const myname = 'Alex'
+// using consts to store variabes that we may forget like colors we used during Let's Read
+const COLOR_WEIRD_BLUE = '#006AFF'; //uppercase because before exectuion
+alert(COLOR_WEIRD_BLUE);
+
+// 
